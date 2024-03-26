b1dd9d7d94934a89d9e7e33fae152ad67db3a822
Ndaziramiyep <patrickofficial30@gmail.com>
2024-03-26 08:08:53 +0200

    Creating professional skills bar

--- script.js
+++ script.js
@@ -34,4 +34,20 @@ let changeText = ()=>{
 };
 
 changeText();
-setInterval(changeText,3000)
+setInterval(changeText,3000);
+
+// circle skill ///////////////////////////////////////
+
+const circles = document.querySelectorAll(".circle");
+circles.forEach(elem =>{
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var parcent = Math.floor(dots*marked/100);
+    var points = "";
+    var rotate = 360 /dots;
+
+    for(let i = 0; i < dots; i++){ 
+         points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
+                    }
+                    elem.innerHTML = points;
+})
