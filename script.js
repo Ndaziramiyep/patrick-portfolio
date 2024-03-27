e55c32a698b497f8ac8e5c5bba2086b6e33c1b7e
Ndaziramiyep <patrickofficial30@gmail.com>
2024-03-27 08:25:38 +0200

    Transilations after responsiveness

--- script.js
+++ script.js
@@ -99,3 +99,25 @@ window.onscroll = ()=>{
     menuIcon.classList.remove("bx-x");
     navlist.classList.remove("open");
 }
+
+// parallax ///////////////////////////////////////
+
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        if(entry.isIntersecting){
+            entry.target.classList.add("show-items");
+        }else{
+        entry.target.classList.remove("show-items");
+    }
+    });
+});
+
+const scrollScale = document.querySelectorAll(".scroll-scale");
+scrollScale.forEach((el)=>observer.observe(el));
+
+const scrollBottom = document.querySelectorAll(".scroll-bottom");
+scrollBottom.forEach((el)=>observer.observe(el));
+
+const scrollTop = document.querySelectorAll(".scroll-top");
+scrollTop.forEach((el)=>observer.observr(el));
+
