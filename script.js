ce3c4586db1d40aa6db20c46d3e8c2fa3e90bbe6
Ndaziramiyep <patrickofficial30@gmail.com>
2024-03-27 06:47:59 +0200

    Web responsiveness

--- script.js
+++ script.js
@@ -60,3 +60,42 @@ circles.forEach(elem =>{
 //  mix it up portfolio section ---------------
 
 var mixer = mixitup('.portfolio-gallery');
+
+
+// active menu ///////////////////////////////////////
+let menuLi = document.querySelectorAll("header ul li a");
+let section = document.querySelectorAll("section");
+
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+        menuLi.forEach(sec => sec.classList.remove("active"));
+        menuLi[len].classList.add("active");
+}
+activeMenu();
+window.addEventListener("scroll", activeMenu);
+
+
+// sticky navbar ///////////////////////////////////////
+
+const header = document.querySelector("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",window.scrollY > 50)
+})
+
+// toggle ico navbar ///////////////////////////////////////
+
+let menuIcon = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".navlist");
+
+menuIcon.onclick = ()=>{
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+}
+
+
+window.onscroll = ()=>{
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+}
