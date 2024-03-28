19d2adb9cfeda0c409100f4a65eabc5f7e1f4303
Ndaziramiyep <patrickofficial30@gmail.com>
2024-03-28 07:10:47 +0200

    Addition of sending email functionality

--- script.js
+++ script.js
@@ -121,3 +121,20 @@ scrollBottom.forEach((el)=>observer.observe(el));
 const scrollTop = document.querySelectorAll(".scroll-top");
 scrollTop.forEach((el)=>observer.observr(el));
 
+// Email Sending ///////////////////////////////////////
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : " ",
+        Password : " ",
+        To :  "patrickofficial30@gmail.com",
+        From : document.getElementById("email").value,
+        Subject : "New Contact From Enquiry",
+        Body : "Name: "+document.getElementById("name").value
+              + "<br> Email:"+document.getElementById("email").value
+              + "<br> Phone No:"+document.getElementById("phone").value
+              + "<br> Message: "+document.getElementById("message").value
+              + "<br>"+document.getElementById("address").value
+          }).then(message => alert("Message Sent Successfully!, Thank You!"));
+}
